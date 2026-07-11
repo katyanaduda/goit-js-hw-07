c1fdfcccbf4215742f6d4d4b2fe1e2a9721b5074
katya_nad <daduda4656@gmail.comexit>
2026-07-11 23:52:47 +0300

    my seventh js-hw

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function list() {
+    const $categories = document.querySelectorAll('ul#categories > li.item');
+
+    console.log($categories.length);
+
+    $categories.forEach(item => {
+        const $item = item.querySelector('h2');
+        console.log(`Category: ${$item.textContent}`);
+        const $items = item.querySelectorAll('ul > li');
+        console.log(`Elements: ${$items.length}`);
+    })
+}
+list();
